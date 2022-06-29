d8543e8af6b0e3ee38b960db843191e327977cca
lol500q <mohmmadayesh22@gmail.com>
2022-06-29 08:00:05 +0300

    deploying

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const app = new Clarifai.App({
          console.log('hi', response)
          if (response) {
            fetch('http://localhost:3000/image', {
-             method: 'post',
+             method: 'put',
              headers: {'Content-Type': 'application/json'},
              body: JSON.stringify({
                id: this.state.user.id
